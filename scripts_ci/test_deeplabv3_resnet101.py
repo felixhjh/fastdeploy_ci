affa73df7458aa480661e6a85add694cc6c87952
felixhjh <852142024@qq.com>
2022-09-22 05:48:06 +0000

    Change test_deeplabv3_resnet50.py test_deeplabv3_resnet101.py

--- scripts_ci/test_deeplabv3_resnet101.py
+++ scripts_ci/test_deeplabv3_resnet101.py
@@ -1,8 +1,8 @@
 from segmentation_test_case_base import *
 
-class TestDeeplabv3_ResNet50_OS8Test(CaseBase):
+class TestDeeplabv3_ResNet101_OS8Test(CaseBase):
     def setup_class(self):
-        self.util = FastdeployTest(data_dir_name="cityscapes", model_dir_name="Deeplabv3_ResNet50_OS8_cityscapes_without_argmax_infer", model_name="deeplabv3_resnet50", csv_path="./infer_result/deeplabv3_resnet50_result.csv")
+        self.util = FastdeployTest(data_dir_name="cityscapes", model_dir_name="Deeplabv3_ResNet101_OS8_cityscapes_without_argmax_infer", model_name="deeplabv3_resnet101", csv_path="./infer_result/deeplabv3_resnet101_result.csv")
         super().setup_class(self)
 
     def set_trt_info(self):
