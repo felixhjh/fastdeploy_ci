1bb76624c0acaec2353697adba2976f070cf88be
yunyaoXYY <imylovex1@163.com>
2022-11-04 02:35:18 +0000

    Add PPOCR Diff CI

---
+++ scripts_ci/test_ppocrv3.py
@@ -0,0 +1,88 @@
+from util import *
+import fastdeploy as fd
+import os
+
+from util import *
+import fastdeploy as fd
+import os
+
+class TestPPOCRv3Test(object):
+    def setup_class(self):
+
+        ####
+        self.util = FastdeployTest(data_dir_name="ICDAR2017", 
+            model_dir_name="PPOCRv3_model", 
+            model_name="PPOCRv3",
+            ground_truth="./infer_result/PPOCRv3_ICDAR2017_10_results.txt",
+            csv_path="./infer_result/PPOCRv3_result.csv")
+
+        # Det Model
+        self.det_model_path = os.path.join(self.util.model_path, "ch_PP-OCRv3_det_infer")  
+        self.det_pdiparams = os.path.join(self.det_model_path, "inference.pdiparams")
+        self.det_pdmodel = os.path.join(self.det_model_path, "inference.pdmodel")
+        # Cls Model
+        self.cls_model_path = os.path.join(self.util.model_path, "ch_PP-ch_ppocr_mobile_v2.0_cls_infer") 
+        self.cls_pdiparams = os.path.join(self.cls_model_path, "inference.pdiparams")
+        self.cls_pdmodel = os.path.join(self.cls_model_path, "inference.pdmodel")
+
+        # Rec Model
+        self.rec_model_path = os.path.join(self.util.model_path, "ch_PP-OCRv3_rec_infer") 
+        self.rec_pdiparams = os.path.join(self.rec_model_path, "inference.pdiparams")
+        self.rec_pdmodel = os.path.join(self.rec_model_path, "inference.pdmodel")
+        self.rec_label_file = os.path.join(self.util.model_path, "ppocr_keys_v1.txt"")
+        
+        self.image_file_path = os.path.join(self.util.data_path, "ICDAR2017_10")
+        self.option = fd.RuntimeOption()
+        self.model_name = self.util.model_name
+        self.csv_save_path = self.util.csv_path
+        self.ground_truth = self.util.ground_truth
+        
+    def teardown_method(self):
+        pass
+    
+    def test_paddle_gpu(self):
+        self.option.use_paddle_backend()
+        self.option.use_gpu()
+
+        det_model = fd.vision.ocr.DBDetector(self.det_pdmodel, self.det_pdiparams, runtime_option=self.option)
+        cls_model = fd.vision.ocr.Classifier(self.cls_pdmodel, self.cls_pdiparams, runtime_option=self.option)
+        rec_model = fd.vision.ocr.Recognizer(
+            self.rec_pdmodel,
+            self.rec_pdiparams,
+            self.rec_label_file,
+            runtime_option=self.option)
+        model = fd.vision.ocr.PPOCRv3(det_model=det_model, cls_model=cls_model, rec_model=rec_model)
+        ppocr_diff_check(model, self.image_file_path, self.ground_truth , model_name=self.model_name, case_name="test_paddle_gpu", csv_path=self.csv_save_path)
+        
+
+    def test_openvino_cpu(self):
+        self.option.use_openvino_backend()
+        self.option.use_cpu()
+
+        det_model = fd.vision.ocr.DBDetector(self.det_pdmodel, self.det_pdiparams, runtime_option=self.option)
+        cls_model = fd.vision.ocr.Classifier(self.cls_pdmodel, self.cls_pdiparams, runtime_option=self.option)
+        rec_model = fd.vision.ocr.Recognizer(
+            self.rec_pdmodel,
+            self.rec_pdiparams,
+            self.rec_label_file,
+            runtime_option=self.option)
+        model = fd.vision.ocr.PPOCRv3(det_model=det_model, cls_model=cls_model, rec_model=rec_model)
+        ppocr_diff_check(model, self.image_file_path, self.ground_truth , model_name=self.model_name, case_name="test_openvino_cpu", csv_path=self.csv_save_path)
+
+
+    def test_ort_gpu(self):
+        self.option.use_ort_backend()
+        self.option.use_gpu(0)
+
+        det_model = fd.vision.ocr.DBDetector(self.det_pdmodel, self.det_pdiparams, runtime_option=self.option)
+        cls_model = fd.vision.ocr.Classifier(self.cls_pdmodel, self.cls_pdiparams, runtime_option=self.option)
+        rec_model = fd.vision.ocr.Recognizer(
+            self.rec_pdmodel,
+            self.rec_pdiparams,
+            self.rec_label_file,
+            runtime_option=self.option)
+        model = fd.vision.ocr.PPOCRv3(det_model=det_model, cls_model=cls_model, rec_model=rec_model)
+        ppocr_diff_check(model, self.image_file_path, self.ground_truth , model_name=self.model_name, case_name="test_ort_gpu", csv_path=self.csv_save_path)
+
+        
+
--- scripts_ci/util.py
+++ scripts_ci/util.py
@@ -48,6 +48,67 @@ def check_result(result_data: dict, ground_truth_data: dict, case_name="", model
     return 0
 
 
+def ppocr_diff_check(ocr_model, image_file_path, txt_path ,model_name="", case_name="", csv_path='./test.csv'):
+    
+    # Prepare Images
+    img_dir = image_file_path
+    imgs_file_lists = []
+    if os.path.isdir(img_dir):
+        for single_file in os.listdir(img_dir):
+            if 'jpg' in single_file:
+                file_path = os.path.join(img_dir, single_file)
+                if os.path.isfile(file_path):
+                    imgs_file_lists.append(file_path)
+    imgs_file_lists.sort()
+
+    # Read local result from txt file
+    local_result=[]
+    with open(txt_path,'r') as f:
+        for line in f:
+            local_result.append(list(map(float,line.split(','))))
+
+    # PPOCR Predict
+    fd_result=[]
+    for idx , image in enumerate(imgs_file_lists):
+        
+        img = cv2.imread(image)
+        result = ocr_model.predict(img)
+        #处理结果
+        for i in range(len(result.boxes)):
+            one_res = result.boxes[i] + [result.rec_scores[i]] + [result.cls_labels[i]] + [result.cls_scores[i]]
+            fd_result.append(one_res) 
+
+    # Begin to Diff Compare
+    total_num_res = len(local_result)*11
+    total_diff_num = 0
+
+    print("==== Begin to check OCR diff ====")
+    for list_local, list_fd in zip (local_result, fd_result):
+
+        for i in range(len(list_local)):
+            
+            if(i < 8):
+                #Det
+                diff = list_local[i] - list_fd[i]
+                write2excel(model_name, case_name, list_fd[i], list_local[i], diff, csv_path)
+                assert(abs(diff) < 1),"Diff exist in Det box result, where is {} - {} .".format(list_local,list_fd)
+            elif (i == 8):
+                #rec
+                diff = round(list_local[i],6) - round(list_fd[i],6)
+                write2excel(model_name, case_name, list_fd[i], list_local[i], diff, csv_path)
+                assert(abs(diff) < 0.00001),"Diff exist in rec scores result, where is {} - {} .".format(list_local,list_fd)
+            elif (i == 9):
+                diff = list_local[i] - list_fd[i]
+                write2excel(model_name, case_name, list_fd[i], list_local[i], diff, csv_path)
+                assert(abs(diff) != 1),"Diff exist in cls label result, where is {} - {} .".format(list_local,list_fd)
+            else:
+                diff = round(list_local[i],6) - round(list_fd[i],6)
+                write2excel(model_name, case_name, list_fd[i], list_local[i], diff, csv_path)s
+                assert(abs(diff) < 0.00001),"Diff exist in cls score result, where is {} - {} .".format(list_local,list_fd)
+    print("==== Finish PPOCR diff check ==== ")
+                
+
+
 def write2excel(model_name, case_name, result_value, ground_truth_val, diff, file_path):
     import csv
     path=file_path
