5bb78291996f19b2ae6a927b168f520f37cfb7c5
yunyaoXYY <imylovex1@163.com>
2022-12-01 08:04:45 +0000

    Add CI backends for ppocrv2/v3

--- scripts_ci/test_ppocrv3.py
+++ scripts_ci/test_ppocrv3.py
@@ -49,7 +49,6 @@ class TestPPOCRv3Test(object):
         pass
     
     
-
     def test_ort_cpu(self):
         self.option.use_ort_backend()
         self.option.use_cpu()
@@ -79,36 +78,6 @@ class TestPPOCRv3Test(object):
         model = fd.vision.ocr.PPOCRv3(det_model=det_model, cls_model=cls_model, rec_model=rec_model)
         ppocr_diff_check(model, self.image_file_path, self.local_result_path , model_name=self.model_name, case_name="test_ort_gpu", csv_path=self.csv_save_path)
     
-
-    def test_trt_gpu(self):
-        self.option.use_trt_backend()
-        self.option.use_gpu()
-
-        # Det
-        det_option = self.option
-        det_option.set_trt_input_shape("x", [1, 3, 64, 64], [1, 3, 640, 640],
-                               [1, 3, 960, 960])
-        det_model = fd.vision.ocr.DBDetector(self.det_pdmodel, self.det_pdiparams, runtime_option=det_option)
-
-        # Cls
-        cls_option = self.option
-        cls_option.set_trt_input_shape("x", [1, 3, 48, 10], [10, 3, 48, 320],
-                               [64, 3, 48, 1024])
-        cls_model = fd.vision.ocr.Classifier(self.cls_pdmodel, self.cls_pdiparams, runtime_option=cls_option)
-
-        # Rec
-        rec_option = self.option
-        rec_option.set_trt_input_shape("x", [1, 3, 48, 10], [10, 3, 48, 320],
-                               [64, 3, 48, 2304])
-        rec_model = fd.vision.ocr.Recognizer(
-            self.rec_pdmodel,
-            self.rec_pdiparams,
-            self.rec_label_file,
-            runtime_option=rec_option)
-        
-
-        model = fd.vision.ocr.PPOCRv3(det_model=det_model, cls_model=cls_model, rec_model=rec_model)
-        ppocr_diff_check(model, self.image_file_path, self.local_result_path , model_name=self.model_name, case_name="test_trt_gpu", csv_path=self.csv_save_path)
 
     def test_paddle_gpu(self):
         self.option.use_paddle_backend()
@@ -152,7 +121,37 @@ class TestPPOCRv3Test(object):
             runtime_option=self.option)
         model = fd.vision.ocr.PPOCRv3(det_model=det_model, cls_model=cls_model, rec_model=rec_model)
         ppocr_diff_check(model, self.image_file_path, self.local_result_path , model_name=self.model_name, case_name="test_openvino_cpu", csv_path=self.csv_save_path)
-    
+
+
+    def test_trt_gpu(self):
+        self.option.use_trt_backend()
+        self.option.use_gpu()
+
+        # Det
+        det_option = self.option
+        det_option.set_trt_input_shape("x", [1, 3, 64, 64], [1, 3, 640, 640],
+                               [1, 3, 960, 960])
+        det_model = fd.vision.ocr.DBDetector(self.det_pdmodel, self.det_pdiparams, runtime_option=det_option)
+
+        # Cls
+        cls_option = self.option
+        cls_option.set_trt_input_shape("x", [1, 3, 48, 10], [10, 3, 48, 320],
+                               [64, 3, 48, 1024])
+        cls_model = fd.vision.ocr.Classifier(self.cls_pdmodel, self.cls_pdiparams, runtime_option=cls_option)
+
+        # Rec
+        rec_option = self.option
+        rec_option.set_trt_input_shape("x", [1, 3, 48, 10], [10, 3, 48, 320],
+                               [64, 3, 48, 2304])
+        rec_model = fd.vision.ocr.Recognizer(
+            self.rec_pdmodel,
+            self.rec_pdiparams,
+            self.rec_label_file,
+            runtime_option=rec_option)
+        
+
+        model = fd.vision.ocr.PPOCRv3(det_model=det_model, cls_model=cls_model, rec_model=rec_model)
+        ppocr_diff_check(model, self.image_file_path, self.local_result_path , model_name=self.model_name, case_name="test_trt_gpu", csv_path=self.csv_save_path)
 
 
     
