900d40b594ae9d51806c03bb617d7836cd56f5dd
yeliang2258 <1047690002@qq.com>
2023-02-16 07:47:12 +0000

    fix deeplabv3_resnet101

--- scripts_ci/test_deeplabv3_resnet101.py
+++ scripts_ci/test_deeplabv3_resnet101.py
@@ -10,4 +10,10 @@ class TestDeeplabv3_ResNet101_OS8Test(CaseBase):
         self.option.use_gpu(0)
         self.option.set_trt_input_shape("x", [1, 3, 112, 112], [1, 3, 512, 1024], [1, 3, 1024, 2048])
 
+    @pytest.mark.skipif(TEST_NNADAPTER=="OFF", reason="Test NNADAPTER is OFF.")
+    def test_nnadapter(self):
+        getattr(self.option, TEST_NNADAPTER)()
+        result = self.run_predict()
+        ret = check_result(result, self.util.ground_truth, "test_nnadapter", self.model_name, 1e-2, self.csv_save_path)
+
 
